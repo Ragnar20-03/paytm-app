a2878805b9cf9f54122e9506d8bb7b22dcfdb96f
Ragnar20-03 <roshanpp20@gmail.com>
2024-08-17 00:36:40 +0530

    Added Prisma ORM and Bootstrap the Starter

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { Button } from "@repo/ui/button";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+   <div className="bg-slate-900 text-white p-3 m-3 text-slate-400">
+    Paytm APp
+   </div>
+  );
+}
